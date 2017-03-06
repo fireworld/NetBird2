09f390b4598c9d0175044b17bb6b7cd2492f6f36
cxx <xx.ch@outlook.com>
2017-03-06 17:09:18 +0800

    1. developing

--- netbird2/src/main/java/cc/colorcat/netbird2/ResponseBody.java
+++ netbird2/src/main/java/cc/colorcat/netbird2/ResponseBody.java
@@ -1,5 +1,6 @@
 package cc.colorcat.netbird2;
 
+import java.io.ByteArrayInputStream;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
@@ -49,8 +50,39 @@ public abstract class ResponseBody implements Closeable {
         Utils.close(stream());
     }
 
+    public static ResponseBody create(String content) {
+        byte[] bytes = content.getBytes();
+        return create(bytes);
+    }
+
+    public static ResponseBody create(String content, String contentType) {
+        Charset charset = Utils.parseCharset(contentType);
+        byte[] bytes = charset != null ? content.getBytes(charset) : content.getBytes();
+        return create(new ByteArrayInputStream(bytes), bytes.length, contentType, charset);
+    }
+
+    public static ResponseBody create(String content, String contentType, Charset charset) {
+        Charset c = Utils.nullElse(charset, Utils.parseCharset(contentType));
+        byte[] bytes = c != null ? content.getBytes(c) : content.getBytes();
+        return create(new ByteArrayInputStream(bytes), bytes.length, contentType, c);
+    }
+
+    public static ResponseBody create(byte[] content) {
+        return create(content, null, null);
+    }
+
+    public static ResponseBody create(byte[] content, String contentType) {
+        if (content == null) throw new NullPointerException("content == null");
+        return create(new ByteArrayInputStream(content), content.length, contentType);
+    }
+
+    public static ResponseBody create(byte[] content, String contentType, Charset charset) {
+        if (content == null) throw new NullPointerException("content == null");
+        return create(new ByteArrayInputStream(content), content.length, contentType, charset);
+    }
+
     public static ResponseBody create(final InputStream is) {
-        return create(is, null);
+        return create(is, -1L, null, null);
     }
 
     public static ResponseBody create(final InputStream is, final String contentType) {
@@ -58,28 +90,7 @@ public abstract class ResponseBody implements Closeable {
     }
 
     public static ResponseBody create(final InputStream is, final long contentLength, final String contentType) {
-        if (is == null) throw new NullPointerException("is == null");
-        return new ResponseBody() {
-            @Override
-            public long contentLength() {
-                return contentLength;
-            }
-
-            @Override
-            public String contentType() {
-                return contentType;
-            }
-
-            @Override
-            public Charset charset() {
-                return Utils.parseCharset(contentType);
-            }
-
-            @Override
-            public InputStream stream() {
-                return is;
-            }
-        };
+        return create(is, contentLength, contentType, Utils.parseCharset(contentType));
     }
 
     public static ResponseBody create(
